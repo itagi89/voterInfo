05d4125077216665a0f67d96dcde6eaddbd90750
itagi89 <Mallikarjun@ITAGI>
2023-03-30 08:24:24 +0530

    adding web project

---
+++ voterInfoWeb/Controllers/VoterDetailsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VoterDetails.BL.Service;
+
+namespace VoterInfoWeb.Controllers
+{
+    public class VoterDetailsController : BaseController
+    {
+        private readonly ILogger<VoterDetailsController> _logger;
+        private readonly IVoterDetails _voterDetails;
+
+        public VoterDetailsController(ILogger<VoterDetailsController> logger, IVoterDetails voterDetails)
+        {
+            _logger = logger;
+            _voterDetails = voterDetails;
+        }
+
+        // GET api/<VoterDetailsController>/5
+        [HttpGet]
+        public async Task<IActionResult> Get(string voterId, string name)
+        {
+            try
+            {
+                var listResponse = await _voterDetails.GetVoterDeatilsById(voterId, name);
+                return CustomResponse(listResponse);
+            }
+            catch (Exception ex)
+            {
+                return ExceptionResponse(ex);
+            }
+        }
+
+
+
+    }
+}
---
+++ voterInfoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VoterInfoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
